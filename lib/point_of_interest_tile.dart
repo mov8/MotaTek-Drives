4256fd2e4d767c5dfec60bbb6dcdbe0364aa543b
James Seddon <james@eggxactly.com>
2024-06-07 09:15:51 +0100

    Save to Sqlite 2nd attempt

--- lib/point_of_interest_tile.dart
+++ lib/point_of_interest_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:drives/models.dart';
+import 'package:drives/services/db_helper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
@@ -33,9 +34,9 @@ class PointOfInterestController {
     _pointOfInterestTileState?.save(id);
   }
 
-  void expand(bool state) {
+  void expand(bool state, bool canEdit) {
     assert(isAttached, 'Controller must be atexpandtached to widget');
-    _pointOfInterestTileState?.expand(state);
+    _pointOfInterestTileState?.expand(state, canEdit);
   }
 }
 
@@ -47,16 +48,18 @@ class PointOfInterestTile extends StatefulWidget {
   final Function onExpandChange;
   final Key key;
   final bool expanded;
+  final bool canEdit;
 
-  const PointOfInterestTile(
-      {required this.key,
-      required this.pointOfInterestController,
-      required this.index,
-      required this.pointOfInterest,
-      required this.onIconTap,
-      required this.onExpandChange,
-      this.expanded = false})
-      : super(key: key);
+  const PointOfInterestTile({
+    required this.key,
+    required this.pointOfInterestController,
+    required this.index,
+    required this.pointOfInterest,
+    required this.onIconTap,
+    required this.onExpandChange,
+    this.expanded = false,
+    this.canEdit = true,
+  }) : super(key: key);
   @override
   State<PointOfInterestTile> createState() => _PointOfInterestTileState();
 }
@@ -68,6 +71,7 @@ class _PointOfInterestTileState extends State<PointOfInterestTile> {
   late String images;
   late int index;
   bool expanded = false;
+  bool canEdit = true;
   // final _key = GlobalKey();
 
   @override
@@ -77,6 +81,7 @@ class _PointOfInterestTileState extends State<PointOfInterestTile> {
 
     /// add to controller if instantiated
     expanded = widget.expanded;
+    canEdit = widget.canEdit;
     index = widget.index;
     name = widget.pointOfInterest.name;
     description = widget.pointOfInterest.description;
@@ -135,34 +140,51 @@ class _PointOfInterestTileState extends State<PointOfInterestTile> {
                   padding: const EdgeInsets.fromLTRB(5, 5, 0, 30),
                   child: Column(children: <Widget>[
                     Row(children: [
-                      Expanded(
+                      if (canEdit) ...[
+                        Expanded(
+                            flex: 10,
+                            child: DropdownButtonFormField<String>(
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Type',
+                                ),
+                                value: type.toString(),
+                                items: poiTypes
+                                    .map((item) => DropdownMenuItem<String>(
+                                          value: item['id'].toString(),
+                                          child: Row(children: [
+                                            Icon(
+                                              IconData(item['iconMaterial'],
+                                                  fontFamily: 'MaterialIcons'),
+                                              color:
+                                                  Color(item['colourMaterial']),
+                                            ),
+                                            Text('    ${item['name']}')
+                                          ]),
+                                        ))
+                                    .toList(),
+                                onChanged: (item) => type =
+                                    item == null ? -1 : int.parse(item))),
+                        const Expanded(
+                          flex: 8,
+                          child: SizedBox(),
+                        ),
+                      ] else ...[
+                        Expanded(
                           flex: 10,
-                          child: DropdownButtonFormField<String>(
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Type',
-                              ),
-                              value: type.toString(),
-                              items: poiTypes
-                                  .map((item) => DropdownMenuItem<String>(
-                                        value: item['id'].toString(),
-                                        child: Row(children: [
-                                          Icon(
-                                            IconData(item['iconMaterial'],
-                                                fontFamily: 'MaterialIcons'),
-                                            color:
-                                                Color(item['colourMaterial']),
-                                          ),
-                                          Text('    ${item['name']}')
-                                        ]),
-                                      ))
-                                  .toList(),
-                              onChanged: (item) =>
-                                  type = item == null ? -1 : int.parse(item))),
-                      const Expanded(
-                        flex: 8,
-                        child: SizedBox(),
-                      ),
+                          child: Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 5, 5, 20),
+                              child: Row(children: [
+                                Icon(
+                                  IconData(poiTypes[type]['iconMaterial'],
+                                      fontFamily: 'MaterialIcons'),
+                                  color:
+                                      Color(poiTypes[type]['colourMaterial']),
+                                ),
+                                Text('    ${poiTypes[type]['name']}')
+                              ])),
+                        ),
+                      ],
                     ]),
                     Row(
                       children: [
@@ -171,6 +193,7 @@ class _PointOfInterestTileState extends State<PointOfInterestTile> {
                                 padding:
                                     const EdgeInsets.fromLTRB(0, 10, 10, 10),
                                 child: TextFormField(
+                                    readOnly: !canEdit,
                                     initialValue: name,
                                     textAlign: TextAlign.start,
                                     keyboardType: TextInputType.streetAddress,
@@ -195,6 +218,9 @@ class _PointOfInterestTileState extends State<PointOfInterestTile> {
                           child: Padding(
                               padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
                               child: TextFormField(
+                                  readOnly: !canEdit,
+                                  maxLines: null,
+                                  //     expands: true,
                                   initialValue: description,
                                   textAlign: TextAlign.start,
                                   keyboardType: TextInputType.streetAddress,
@@ -270,17 +296,9 @@ class _PointOfInterestTileState extends State<PointOfInterestTile> {
                   '$directory/point_of_interest_${id}_$num.${pickedFile.path.split('.').last}';
               File(pickedFile.path).copy(imagePath);
               setState(() {
-                //    if (num == 1) {
                 images =
                     '[${images.isNotEmpty ? '${images.substring(1, images.length - 1)},' : ''}{"url":"$imagePath","caption":"image $num"}]';
                 debugPrint('Images: $images');
-                /*        } else {
-                String images2 =
-                    '[${images.isNotEmpty ? '${images.substring(1, images.length - 1)},' : ''}{"url":"$imagePath","caption":"image $num"}]';
-                debugPrint('Images2: $images2');
-      
-              }
-      */
               });
             }
           } catch (e) {
@@ -308,7 +326,7 @@ class _PointOfInterestTileState extends State<PointOfInterestTile> {
     }
   }
 
-  expand(bool state) {
+  expand(bool state, bool canEdit) {
     expanded = state;
   }
 }
